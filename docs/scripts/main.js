026a6f36e6c9692afe696d922cf017566fa491c4
TomaszBudny <tomasz.budny.89@gmail.com>
2018-09-01 03:37:47 +0200

    drobne

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -74,5 +74,19 @@
       resetFixedNav($('.header-navigation'), 'top', navTop, $('header.banner'));
     }, 300);
   });
-
+  jQuery('a[href*=#]:not([href=#])').click(function() {
+    if (jQuery(this).hasClass('carousel-control')) {
+      return;
+    }
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = jQuery(this.hash);
+      target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        jQuery('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
 })(jQuery);
